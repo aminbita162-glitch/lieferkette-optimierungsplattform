3be83f7ecc17d9ef44d2c95c96503ad1e9e02946
aminbita162-glitch <aminbita162@icloud.com>
2026-03-09 22:20:49 +0100

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    full_name = Column(String, nullable=True)
+    hashed_password = Column(String, nullable=False)
+    disabled = Column(Boolean, default=False)
