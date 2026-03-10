69b0a8f9045e88dee038b43d8ba0da1f72becfe6
aminbita162-glitch <aminbita162@icloud.com>
2026-03-10 12:01:53 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,6 +5,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
